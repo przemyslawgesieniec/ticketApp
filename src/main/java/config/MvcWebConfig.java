59f187450720addcbde8ae3ec2073608386583ec
kapj18-50 <kapj18-50@sr.dmcs.pl>
2018-12-02 19:42:07 +0100

    thymleaf working

--- src/main/java/config/MvcWebConfig.java
+++ src/main/java/config/MvcWebConfig.java
@@ -1,31 +1,70 @@
 package main.java.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"main.java.controllers"})
 public class MvcWebConfig implements WebMvcConfigurer
 {
-    @Bean
-    public ViewResolver viewResolver(){
-        InternalResourceViewResolver vr = new InternalResourceViewResolver();
-        vr.setPrefix("");
-        vr.setSuffix(".html");
-        return vr;
-    }
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
 
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("/WEB-INF/static/");
+    }
+
+    /**
+     * Allow using static resources
+     */
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+
+    /**
+     *Thymleaf template view resolver
+     */
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("WEB-INF/static/html/");
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+    public void configureViewResolvers(ViewResolverRegistry registry)
+    {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        registry.viewResolver(resolver);
     }
 
 }
--- src/main/java/controllers/IndexController.java
+++ src/main/java/controllers/IndexController.java
@@ -5,12 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 @Controller
 public class IndexController {
 
     @RequestMapping(value={"/index",""}, method = RequestMethod.GET)
     public String index(Model model) {
 
-        return "static/html/index";
+        model.addAttribute("name", "adam");
+        return "index";
     }
 }
